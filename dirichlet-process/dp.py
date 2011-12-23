7bb614953b8f903d3372c846fa57c361fe1078d9
Alireza Nourian <alireza.nournia@gmail.com>
2011-12-23 19:08:34 +0330

    add effect of cluster count

--- dirichlet-process/dp.py
+++ dirichlet-process/dp.py
@@ -15,7 +15,7 @@ X = len(Y) * [Y.mean()]
 
 # parameters
 muG, sigmaG = Y.mean(), Y.std() # G0 ~ N(muG, sigmaG) # best guess of G
-A0 = 0.01 # strength of belief that G0 is G
+A0 = 0.001 # strength of belief that G0 is G
 
 def makeClusters(): # make cluster dictionary from data and labels
 	clusters = {}
@@ -38,24 +38,21 @@ def getLabelOfNewCluster(y, mu0):
 	return np.random.normal((mu0 + y)/2, 2**-.5) # sample Gausian distribution
 
 # iteration
-iterations = 20
+iterations = 100
 for itr in range(1, iterations+1):
 	clusters = makeClusters()
 
 	#update temperature
-	T = .3 + np.random.uniform(5.*itr/iterations, (10.*itr/iterations)**2)
+	T = .1 + np.random.uniform(2.*itr/iterations, (10.*itr/iterations)**2)
 	# print T; continue
 
 	# update X[i] in n+1 way using the multinomial selector
 	for i in range(len(Y)):
 		probabilites = [] # of each condition
 		for label, items in clusters.iteritems():
-			probabilites.append(phi(Y[i] - label))
+			probabilites.append(phi(Y[i] - label) * len(items))
 		probabilites.append(A(Y[i])**T)
 		probabilites = np.array(probabilites)
-
-		# simulated annealing effect
-		# probabilites = probabilites**T
 		
 		# sum of probabilites equals to 1
 		probabilites /= probabilites.sum()
@@ -72,6 +69,6 @@ for itr in range(1, iterations+1):
 	# print results
 	clusters = makeClusters()
 	print
+	print 'iteration: {}, clusters: {}, temperature: {:.2f}'.format(itr, len(clusters), T)
 	for label in sorted(clusters.iterkeys()):
 		print '{:.2f} -> {}'.format(label, clusters[label])
-	print 'iteration: {}, clusters: {}, temperature: {}'.format(itr, len(clusters), T)
